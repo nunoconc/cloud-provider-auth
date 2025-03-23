53e8440dd14e588edef1d0c4520d7910554a76a7
nunoconc <nmpconceicao@gmail.com>
2025-03-23 22:37:45 +0000

    feat: add webauthn with passkey login

--- express-server/routes/auth.js
+++ express-server/routes/auth.js
@@ -1,13 +1,21 @@
 import express from 'express';
 
+import {
+    generateAuthenticationOptions,
+    generateRegistrationOptions, verifyAuthenticationResponse, verifyRegistrationResponse,
+} from '@simplewebauthn/server';
+
 const router = express.Router();
 import {Low} from 'lowdb';
 import {JSONFile} from 'lowdb/node';
 import bcrypt from 'bcryptjs';
 import {jwtDecode} from "jwt-js-decode";
+import base64url from "base64url";
 
 const file = new JSONFile('db.json');
 const db = new Low(file, {users: []});
+const rpID = 'localhost'; // relaying party identifier
+const expectedOrigin = `http://${rpID}:3000`;
 
 router.get('/login', function (req, res) {
     res.render('login', {title: 'Login Page'});
@@ -78,19 +86,117 @@ router.post('/login-google', async function (req, res) {
 })
 
 router.post('/auth-options', async function (req, res) {
+    // Read data from JSON file, this will set db.data content
+    await db.read();
+
     const foundUser = db.data.users.find(u => u.email === req.body.email);
     if (foundUser) {
         res.send({
-            password : foundUser.password !== false,
+            password: foundUser.password !== false,
             google: foundUser.federated && foundUser.federated.google,
             webauthn: foundUser.webauthn,
         });
     } else {
         res.send({
-            password : true,
+            password: true,
         });
     }
 })
+
+router.post("/webauthn-login-options", async (req, res) => {
+    // Read data from JSON file, this will set db.data content
+    await db.read();
+
+    const user = db.data.users.find(u => u.email === req.body.email);
+
+    //if (user === null) {
+    //    res.sendStatus(404);
+    //    return;
+    //}
+
+    const options = {
+        timeout: 60000,
+        allowCredentials: [],
+        devices: user && user.devices ? user.devices.map(dev => ({
+            id: dev.credentialID,
+            type: 'public-key',
+            transports: dev.transports,
+        })) : [],
+        userVerification: 'required',
+        rpID,
+    };
+
+    const loginOpts = await generateAuthenticationOptions(options);
+
+    if (user) {
+        user.currentChallenge = loginOpts.challenge;
+    }
+
+    res.send(loginOpts);
+});
+
+router.post("/webauthn-login-verification", async (req, res) => {
+    // Read data from JSON file, this will set db.data content
+    await db.read();
+
+    const data = req.body.data;
+
+    const user = db.data.users.find(u => u.email === req.body.email);
+
+    if (user == null) {
+        res.sendStatus(400).send({ok: false});
+        return;
+    }
+
+    const expectedChallenge = user.currentChallenge;
+
+    let dbAuthenticator;
+    const bodyCredIDBuffer = base64url.toBuffer(data.rawId);
+
+    for (const dev of user.devices) {
+        const currentCredential = new Buffer(dev.credentialID.data);
+        if (bodyCredIDBuffer.equals(currentCredential)) {
+            dbAuthenticator = dev;
+            break;
+        }
+    }
+
+    if (!dbAuthenticator) {
+        return res.status(400).send({ok: false, message: 'Authenticator is not registered with this site'});
+    }
+
+    let verification;
+    try {
+        const options = {
+            credential: data,
+            expectedChallenge: `${expectedChallenge}`,
+            expectedOrigin,
+            expectedRPID: rpID,
+            authenticator: {
+                ...dbAuthenticator,
+                credentialPublicKey: new Buffer(dbAuthenticator.credentialPublicKey.data) // Re-convert to Buffer from JSON
+            },
+            requireUserVerification: true,
+        };
+        verification = await verifyAuthenticationResponse(options);
+    } catch (error) {
+        return res.status(400).send({ok: false, message: error.toString()});
+    }
+
+    const {verified, authenticationInfo} = verification;
+
+    if (verified) {
+        dbAuthenticator.counter = authenticationInfo.newCounter;
+    }
+
+    res.send({
+        ok: true,
+        user: {
+            name: user.name,
+            email: user.email
+        }
+    });
+});
 
 router.post('/register', async function (req, res) {
     const salt = bcrypt.genSaltSync(10);
@@ -125,4 +231,102 @@ router.post('/register', async function (req, res) {
     }
 })
 
+router.post("/webauthn-registration-options", async (req, res) => {
+    // Read data from JSON file, this will set db.data content
+    await db.read();
+
+    const user = db.data.users.find(u => u.email === req.body.email);
+
+    const options = {
+        rpName: 'cloud-provider-auth',
+        rpID,
+        userName: user.email,
+        timeout: 60000,
+        attestationType: 'none',
+
+        /**
+         * Passing in a user's list of already-registered authenticator IDs here prevents users from
+         * registering the same device multiple times. The authenticator will simply throw an error in
+         * the browser if it's asked to perform registration when one of these ID's already resides
+         * on it.
+         */
+        excludeCredentials: user.devices ? user.devices.map(dev => ({
+            id: dev.credentialID,
+            type: 'public-key',
+            transports: dev.transports,
+        })) : [],
+
+        authenticatorSelection: {
+            userVerification: 'required',
+            residentKey: 'required',
+        },
+        /**
+         * The two most common algorithms: ES256, and RS256
+         */
+        supportedAlgorithmIDs: [-7, -257],
+    };
+
+    /**
+     * The server needs to temporarily remember this value for verification, so don't lose it until
+     * after you verify an authenticator response.
+     */
+    const regOptions = await generateRegistrationOptions(options)
+    user.currentChallenge = regOptions.challenge;
+    await db.write();
+
+    res.send(regOptions);
+});
+
+router.post("/webauthn-registration-verification", async (req, res) => {
+    // Read data from JSON file, this will set db.data content
+    await db.read();
+
+    const user = db.data.users.find(u => u.email === req.body.email);
+    const data = req.body.data;
+
+    const expectedChallenge = user.currentChallenge;
+
+    let verification;
+    try {
+        const options = {
+            response: data,
+            expectedChallenge: `${expectedChallenge}`,
+            expectedOrigin,
+            expectedRPID: rpID,
+            requireUserVerification: true,
+        };
+        verification = await verifyRegistrationResponse(options);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({error: error.toString()});
+    }
+
+    const {verified, registrationInfo} = verification;
+
+    if (verified && registrationInfo) {
+        const {credentialPublicKey, credentialID, counter} = registrationInfo;
+
+        const existingDevice = user.devices ? user.devices.find(
+            device => new Buffer(device.credentialID.data).equals(credentialID)
+        ) : false;
+
+        if (!existingDevice) {
+            const newDevice = {
+                credentialPublicKey,
+                credentialID,
+                counter,
+                transports: data.response.transports,
+            };
+            if (user.devices === undefined) {
+                user.devices = [];
+            }
+            user.webauthn = true;
+            user.devices.push(newDevice);
+            await db.write();
+        }
+    }
+
+    res.send({ok: true});
+
+});
 export default router;
